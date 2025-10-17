7dcd829967ef2c92c013cd4ff4cae0d0eb5d9028
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-10-17 16:23:00 +0530

    multiple changes

--- server/dist/index.js
+++ server/dist/index.js
@@ -23,11 +23,11 @@ async function sendOtpEmail(to, otp) {
         },
     });
     await transporter.sendMail({
-        from: `"Auction Auth" <${process.env.EMAIL_USER}>`,
+        from: "Verify your Email",
         to,
-        subject: "Your OTP Code",
+        subject: "Please use this One Time Password (OTP) to verify your mail id.",
         text: `Your OTP is ${otp}`,
-        html: `<p>Your OTP is <b>${otp}</b>. It will expire in 5 minutes.</p>`,
+        html: `<p>Your OTP is <b>${otp}</b>. It will expire in 5 minutes. Please don't share it to anyone, for your own safety and privacy.</p>`,
     });
 }
 app.get('/test', (req, res) => {
@@ -51,7 +51,7 @@ app.post('/otp-sender', async (req, res) => {
         return res.status(400).json({ success: false, message: 'Invalid email address' });
     }
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    const expiresAt = (Date.now() + 5 * 60 * 1000).toLocaleString(); // 5 minutes
+    const expiresAt = (Date.now() + 5 * 60 * 1000); // 5 minutes
     otpStore[contact] = { otp, expiresAt, attempts: 0 };
     console.log(otpStore);
     try {
@@ -64,6 +64,29 @@ app.post('/otp-sender', async (req, res) => {
     catch (error) {
         console.error("Error sending OTP:", error);
         return res.status(500).json({ success: false, message: 'Failed to send OTP email' });
+    }
+});
+app.post('/otp-verify', async (req, res) => {
+    const otp = req.body.otp;
+    const contact = req.body.contact;
+    if (!otp || !contact) {
+        return res.status(404).send("No OTP or Contact number received.");
+    }
+    const record = otpStore[contact];
+    console.log(record);
+    console.log(otp, record);
+    if (!record)
+        res.status(400).send({ success: false, message: 'No record for OTP found for this contact.' });
+    else if (Date.now() > record.expiresAt) {
+        res.status(400).send({ success: false, message: 'OTP has been expired.' });
+    }
+    else if (otp === record.otp) {
+        res.status(200).send({ success: true, message: "OTP Verified" });
+    }
+    else if (otp != record.otp) {
+        {
+            res.status(400).send({ success: false, message: "Wrong OTP" });
+        }
     }
 });
 const PORT = process.env.PORT;
--- server/src/index.ts
+++ server/src/index.ts
@@ -84,5 +84,32 @@ app.post('/otp-sender',async (req:Request,res:Response) => {
     }
 })
 
+app.post('/otp-verify',async (req:Request,res:Response) => {
+    const otp = req.body.otp;
+    const contact = req.body.contact;
+
+    if(!otp || !contact) {
+        return res.status(404).send("No OTP or Contact number received.")
+    }
+    const record = otpStore[contact];
+    console.log(record);
+    console.log(otp,record)
+
+    if(!record) res.status(400).send({success:false,message:'No record for OTP found for this contact.'})
+    else if(Date.now() > record.expiresAt){
+        res.status(400).send({success:false,message:'OTP has been expired.'})
+    }
+    else if(otp === record.otp){
+        res.status(200).send({success:true,message:"OTP Verified"})
+    }
+    else if(otp != record.otp){{
+        res.status(400).send({success:false,message:"Wrong OTP"})
+    }}
+    else{
+        res.status(400).send({success:false,message:"Unexpected error occured"})
+    }
+
+})
+
 const PORT = process.env.PORT;
 app.listen(PORT,() => console.log(`server is running at http://localhost:${PORT}/test`))
