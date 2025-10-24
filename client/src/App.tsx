161d7d50a3a1160f773841f201893f521da1e82b
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-10-25 02:07:36 +0530

    UX enhanced by custom message on login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 function App() {
+  const [isReturnedUser,setisReturnedUser] = useState<Boolean | null>(null);
 
+  useEffect(() => {
+
+    if(localStorage.getItem("hasVisited")) setisReturnedUser(true);
+
+    else{
+      localStorage.setItem("hasVisited","true");
+      setisReturnedUser(false)
+    }
+
+  },[])
+
+  if(isReturnedUser === null) return null;
+  
   return (
     <div className='overflow max-h-screen max-w-screen '>
-      <div className='absolute h-full w-full bg-blue-100/50 backdrop-blur-5xl'></div>
-      
+      <div className='absolute h-full w-full bg-blue-100/50 backdrop-blur-5xl flex justify-center items-center'>
+        <div className=' rounded-xl p-10 bg-white/90 shadow-lg flex flex-col '>
+          <span className='font-mono font-semibold text-lg'>{isReturnedUser ? "Welcome back, Captain!" : "Looks like you're new here!"}</span> 
+          <div>
+            
+          </div>
+        </div>
+      </div>
       <div className='fixed blur-[100px] -z-10 -right-200 bg-[#7F30FF]  h-[70vh] w-[1500px] rounded-full rotate-[45deg]'></div>
       <div className='fixed blur-[100px]  -z-10 -right-70 top-100 bg-[#00B0FF]  h-[70vh] w-[800px] rounded-full rotate-[45deg]'></div>
       <div className='fixed blur-[100px]  -z-10 right-70 bg-[#0038FF] top-110  h-[600px] w-[600px] rounded-full'></div> 
       <div className='fixed blur-[100px]  -z-10 right-120 bg-[#FF4880] top-110  h-[80vh] w-[900px] rounded-full rotate-[90deg]'></div> 
       <div className='fixed blur-[100px]  -z-10 -left-70 bg-[#FF8000] top-140  h-[80vh] w-[900px] rounded-full rotate-[90deg]'></div> 
+      
      
     </div>
     
